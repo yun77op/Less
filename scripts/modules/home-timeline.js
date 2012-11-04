14bad613f26a9297764c72bce442726183ca3435
Yunhu Jiang <yun77op@gmail.com>
2012-11-04 23:54:34 +0800

    scoll fetch

--- scripts/modules/home-timeline.js
+++ scripts/modules/home-timeline.js
@@ -21,8 +21,25 @@ define(function(require, exports) {
             });
 
             this.model.on( 'add', this.addOne, this );
-
+            document.onscroll = this._handleScroll.bind(this);
             HomeTimelineModule.__super__['initialize'].apply(this, args);
+        },
+        _handleScroll: function() {
+            var body = document.body;
+            var offset = 100;
+            if (this._scrollFetching || window.innerHeight + body.scrollTop + offset < body.scrollHeight) return;
+
+            var options = {
+                data {
+                    max_id: this.model.last().id
+                },
+                success: function(model, resp) {
+                    this._scrollFetching = false;
+                }
+            };
+
+            this._scrollFetching = true;
+            this.fetch(data);
         },
         _handleUnread: function(count) {
             this.$unreadCount.text('有 ' + count + ' 条新微博，点击查看').show();
@@ -31,10 +48,14 @@ define(function(require, exports) {
             var $status = new StreamItem({ model: status }).render().$el;
             this.$el.find('.stream').prepend($status);
         },
+
         _renderUnread: function() {
+            this.fetch({ since_id: this.model.at(0).id });
+        },
+        fetch: function(data) {
             this.$unreadCount.hide();
 
-            var data = _.extend({}, this.options.data, { since_id: this.model.at(0).id });
+            var data = _.extend({}, this.options.data, data);
             var options = {
                 data: data,
                 add: true
